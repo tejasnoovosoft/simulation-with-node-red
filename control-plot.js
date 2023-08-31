b80ec5faec43dc5ccf221c574ca95929a58d74d3
harshit-noovosoft <harshit.manavadariya@noovosoft.com>
2023-08-31 15:44:50 +0530

    Plot static points in graph

--- control-plot.js
+++ control-plot.js
@@ -1,113 +1,13 @@
+import { InitiateGraph } from "/initial-graph.js";
+
 const svg = document.getElementById('svg3');
-
-function drawPlot(voltage, temperature, timeStamp) {
-    const xAxisMinLimit = 40;
-    const xAxisMaxLimit = 1200;
-    const yAxisMinLimit = 20;
-    const yAxisMaxLimit = 380;
-    let points = [];
-    const xAxisGap = (xAxisMaxLimit - xAxisMinLimit) / temperature.length;
-    const yAxisGap = (yAxisMaxLimit - yAxisMinLimit) / 10;
-    const minYAxisValue = Math.min(Math.min(...temperature), Math.min(...voltage));
-    const maxYAxisValue = Math.max(Math.max(...temperature), Math.max(...voltage));
-    const yAxisLabelGap = Math.ceil((maxYAxisValue - minYAxisValue) / 10);
-    let yLabel = maxYAxisValue;
-
-    function drawAxis() {
-        //X-Axis
-        drawLine(svg, {
-            x1: xAxisMinLimit,
-            y1: yAxisMaxLimit,
-            x2: xAxisMaxLimit,
-            y2: yAxisMaxLimit,
-            stroke: "black",
-            strokeWidth: 1,
-            strokeType: "solid"
-        });
-
-        //Y-Axis
-        drawLine(svg, {
-            x1: xAxisMinLimit,
-            y1: yAxisMinLimit,
-            x2: xAxisMinLimit,
-            y2: yAxisMaxLimit,
-            stroke: "black",
-            strokeWidth: 1,
-            strokeType: "solid"
-        });
-    }
-
-    function drawTicsAndLabel() {
-        // Label and Tics for X-Axis
-        let idx = 1;
-        for (let key in timeStamp) {
-            //Axis-Tics
-            drawLine(svg, {
-                x1: xAxisMinLimit + (xAxisGap * (idx)),
-                y1: yAxisMaxLimit,
-                x2: xAxisMinLimit + (xAxisGap * (idx)),
-                y2: yAxisMaxLimit + 5
-            });
-
-            //Axis-Label
-            drawText(svg, {x: (xAxisMinLimit - 3) + xAxisGap * (idx), y: yAxisMaxLimit + 20, text: `${key}`});
-            idx++;
-        }
-
-        // Label and Tics for Y-Axis
-        for (let i = 0; i <= 10; i++) {
-            //Axis - Tics
-            drawLine(svg, {
-                x1: xAxisMinLimit - 4,
-                y1: yAxisMinLimit + yAxisGap * (i),
-                x2: xAxisMinLimit,
-                y2: yAxisMinLimit + yAxisGap * (i)
-            });
-
-            // Axis-labels
-            drawText(svg, {
-                x: xAxisMinLimit - 30,
-                y: (yAxisMinLimit + 5) + yAxisGap * (i),
-                text: `${yLabel}`
-            });
-            yLabel -= yAxisLabelGap;
-        }
-    }
-
-    function plotDotsOnGraph(dataToBePlot, color) {
-        let idx = 1;
-        for (let key in dataToBePlot) {
-            drawCircle(svg, {
-                cx: xAxisMinLimit + (xAxisGap * (idx)),
-                cy: yAxisMinLimit + ((maxYAxisValue - dataToBePlot[key]) / yAxisLabelGap) * yAxisGap,
-                r: 3,
-                fill: color,
-            });
-            points.push({
-                x: xAxisMinLimit + (xAxisGap * (idx)),
-                y: yAxisMinLimit + ((maxYAxisValue - dataToBePlot[key]) / yAxisLabelGap) * yAxisGap
-            });
-            if (key > 0) {
-                drawLine(svg, {
-                    x1: points[key - 1].x,
-                    y1: points[key - 1].y,
-                    x2: points[key].x,
-                    y2: points[key].y,
-                    stroke: color,
-                    strokeWidth: 2
-                })
-            }
-            idx += 1;
-        }
-        points = [];
-    }
-
-    function drawControlChart() {
-        drawAxis();
-        drawTicsAndLabel();
-        plotDotsOnGraph(temperature, "green");
-        plotDotsOnGraph(voltage, "red");
-    }
-
-    drawControlChart();
+const firstGraphObj = new InitiateGraph(svg,60,12);
+firstGraphObj.drawAxis();
+firstGraphObj.drawXAxisTics();
+firstGraphObj.drawXAxisLabel(1,60);
+firstGraphObj.drawYAxisTics();
+firstGraphObj.drawYAxisLabel(10,50);
+export function drawTemperatureAndVoltageGraph(temperature,voltage,timestamp) {
+    firstGraphObj.plotPoints(temperature,"green");
+    firstGraphObj.plotPoints(voltage,"red");
 }
--- node-red-exercise.js
+++ node-red-exercise.js
@@ -1,55 +1,57 @@
-// Import the functions you need from the SDKs you need
-import {initializeApp} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-import {getDatabase, ref, onValue} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+import { ref , getDatabase , onValue , onChildAdded } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
+import {drawTemperatureAndVoltageGraph} from "/control-plot.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBVpZ4l6gzYc1GDcH4F7jIJqtUQ7m_AM7A",
-    authDomain: "node-red-exercise.firebaseapp.com",
-    databaseURL: "https://node-red-exercise-default-rtdb.firebaseio.com",
-    projectId: "node-red-exercise",
-    storageBucket: "node-red-exercise.appspot.com",
-    messagingSenderId: "1049851901020",
-    appId: "1:1049851901020:web:98e79dc1401493a9b26299",
-    measurementId: "G-MD190Q7EE8"
+    apiKey: "AIzaSyAzi_YhUl1IVZYDbxeYrxuQnSmIHOTlpdE",
+    authDomain: "node-red-noovoso.firebaseapp.com",
+    projectId: "node-red-noovoso",
+    storageBucket: "node-red-noovoso.appspot.com",
+    messagingSenderId: "862897712000",
+    appId: "1:862897712000:web:bb00af9a4420f8ccba6f65"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-const dataRef = ref(db, 'data');
+const database = getDatabase(app);
+const voltageDataRef = ref(database,'Voltage');
+const temperatureDataRef = ref(database,'Temperature');
+const voltageData = [];
+const temperatureData = [];
+const timeStamp = [];
 
-let voltage = [];
-let temperature = [];
-let timeStamp = [];
-
-function clearGraph() {
-    const myNode = document.getElementById("svg3");
-    while (myNode.firstChild) {
-        myNode.removeChild(myNode.lastChild);
-    }
-}
-
-onValue(dataRef, (snapshot) => {
-    voltage = [];
-    timeStamp = [];
-    temperature = [];
-    const FetchData = snapshot.val();
-    const voltageData = FetchData.voltage;
-    const tempData = FetchData.temperature;
-    for (let key in voltageData) {
-        timeStamp.push(key);
-        voltage.push(voltageData[key]);
-    }
-    temperature = [];
-    for (let key in tempData) {
-        temperature.push(tempData[key]);
-    }
-    clearGraph();
-    drawPlot(voltage, temperature, timeStamp);
+onValue(voltageDataRef, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+        voltageData.push(childSnapshot.val());
+    });
+}, {
+    onlyOnce: true
+});
+onValue(temperatureDataRef, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+        temperatureData.push(childSnapshot.val());
+        timeStamp.push(childSnapshot.key);
+    });
+    drawTemperatureAndVoltageGraph(temperatureData,voltageData,timeStamp);
+}, {
+    onlyOnce: true
 });
 
+let promiseForTemp,promiseForVoltage;
+onChildAdded(temperatureDataRef,(data)=>{
+    promiseForTemp = new Promise(function (res , rej){
+        res(data.val());
+    });
+});
 
-
+onChildAdded(voltageDataRef,(data)=>{
+    promiseForVoltage = new Promise(function (res , rej){
+        res(data.val());
+    });
+    promiseForTemp.then(function (value){
+        temperatureData.push(value);
+        promiseForVoltage.then( function (value) {
+            voltageData.push(value)
+            drawTemperatureAndVoltageGraph(temperatureData,voltageData);
+        },function (err){});
+    },function (err){});
+});
